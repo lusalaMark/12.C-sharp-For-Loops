1c6eb5ddab659f9d6a2fcaf9099025439022eab2
Mark Lusala Mudoga <lusalamark63@gmail.com>
2021-03-23 16:39:33 +0300

    First Commit

---
+++ 12.C sharp For Loops/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace _12.C_sharp_For_Loops
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.SuspendLayout();
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+			this.richTextBox1.Location = new System.Drawing.Point(12, 219);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(904, 434);
+			this.richTextBox1.TabIndex = 7;
+			this.richTextBox1.Text = "";
+			this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+			this.label2.Location = new System.Drawing.Point(298, 140);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(168, 63);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "Reset";
+			this.label2.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+			this.label1.Location = new System.Drawing.Point(12, 140);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(99, 63);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "Fill";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(12, 2);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(904, 105);
+			this.progressBar1.Step = 1;
+			this.progressBar1.TabIndex = 4;
+			this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(956, 450);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.progressBar1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ProgressBar progressBar1;
+	}
+}
+
